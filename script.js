e1c29f78c861cf1058449d0883eb5d065ae85f40
1adex <ridwanadebayo358@gmail.com>
2022-06-24 18:55:59 +0100

    first js

---
+++ script.js
@@ -0,0 +1,13 @@
+const firstname = "Ridwan";
+const lastname = "Adebayo";
+
+
+const fullname = firstname + " " + lastname;
+
+let height = "6ft"
+let country = "Nigeria"
+
+
+console.log(fullname)
+console.log(height)
+console.log(country)
